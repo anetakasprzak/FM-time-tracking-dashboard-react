9f3133647a9bcd37cae5640937a22702e73f57b8
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-01-24 17:10:26 +0000

    mobile styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,37 @@ function App() {
   return (
     <div className="wrapper">
       <div className="user__card">
-        <div className="user__details">
-          <img className="user__img" src="../public/images/image-jeremy.png" />
-          <div className="user__text-box">
-            <p className="user__report">Report for</p>
-            <p className="user__name">Jeremy Robson</p>
-          </div>
-        </div>
         <div className="user__timeframes-box">
-          <button
-            onClick={() => setFilter("daily")}
-            className="timeframes__time"
-          >
-            Daily
-          </button>
-          <button
-            onClick={() => setFilter("weekly")}
-            className="timeframes__time"
-          >
-            Weekly
-          </button>
-          <button
-            onClick={() => setFilter("monthly")}
-            className="timeframes__time"
-          >
-            Monthly
-          </button>
+          <div className="user__details">
+            <img
+              className="user__img"
+              src="../public/images/image-jeremy.png"
+            />
+            <div className="user__text-box">
+              <p className="user__report">Report for</p>
+              <p className="user__name">Jeremy Robson</p>
+            </div>
+          </div>
+          <div className="btn__box">
+            <button
+              onClick={() => setFilter("daily")}
+              className="timeframes__btn"
+            >
+              Daily
+            </button>
+            <button
+              onClick={() => setFilter("weekly")}
+              className="timeframes__btn"
+            >
+              Weekly
+            </button>
+            <button
+              onClick={() => setFilter("monthly")}
+              className="timeframes__btn"
+            >
+              Monthly
+            </button>
+          </div>
         </div>
       </div>
       {data?.map((el, i) => (
@@ -49,22 +54,27 @@ function ActivityCard({ el, i, filter }) {
   const isMonthly = filter === "monthly";
 
   return (
-    <div className={`card__box card__box--${i + 1}`}>
-      <div className="title__box">
-        <h4 className="card__title">{el.title}</h4>
-        <img className="card__dots" src="../public/images/icon-ellipsis.svg" />
-      </div>
-      <div className="card__hours-box">
-        <h2 className="card__hours--main">
-          {isDaily && `${el.timeframes.daily.current} hrs`}
-          {isWeekly && `${el.timeframes.weekly.current} hrs`}
-          {isMonthly && `${el.timeframes.monthly.current} hrs`}
-        </h2>
-        <p className="card__hours--previous">
-          {isDaily && `Yesterday - ${el.timeframes.daily.previous} hrs`}
-          {isWeekly && `Last week - ${el.timeframes.weekly.previous} hrs`}
-          {isMonthly && `Last month - ${el.timeframes.monthly.previous} hrs`}
-        </p>
+    <div className={`card__color-wrapper card__color-wrapper--${i + 1}`}>
+      <div className={`card__box card__box--${i + 1}`}>
+        <div className="title__box">
+          <h4 className="card__title">{el.title}</h4>
+          <img
+            className="card__dots"
+            src="../public/images/icon-ellipsis.svg"
+          />
+        </div>
+        <div className="card__hours-box">
+          <h2 className="card__hours--main">
+            {isDaily && `${el.timeframes.daily.current} hrs`}
+            {isWeekly && `${el.timeframes.weekly.current} hrs`}
+            {isMonthly && `${el.timeframes.monthly.current} hrs`}
+          </h2>
+          <p className="card__hours--previous">
+            {isDaily && `Yesterday - ${el.timeframes.daily.previous} hrs`}
+            {isWeekly && `Last week - ${el.timeframes.weekly.previous} hrs`}
+            {isMonthly && `Last month - ${el.timeframes.monthly.previous} hrs`}
+          </p>
+        </div>
       </div>
     </div>
   );
